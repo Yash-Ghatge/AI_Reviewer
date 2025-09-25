7497448ad8eea2b098e878ae6f331a705b49b8c0
Yash-Ghatge <yashghatge012@gmail.com>
2025-09-25 19:09:34 +0530

    Update

--- Backend/server.js
+++ Backend/server.js
@@ -6,21 +6,19 @@ import cors from 'cors'
 const app = express()
 const allowedOrigins = ['http://localhost:5173','https://ai-reviewer-j876.vercel.app','https://ai-reviewer-j876-git-main-yash-ghatges-projects.vercel.app']
 
+app.use(express.json())
+dotenv.config()
+
 app.use(cors({
     origin:allowedOrigins,
     credentials:true
 }));
 
 
-app.use(express.json())
-dotenv.config()
-
-
-app.use('/api/ai',router)
 app.get('/',(req,res)=>{
     res.send("API IS RUNNING")
 })
-
+app.use('/api/ai',router)
 
 app.listen(process.env.PORT, ()=>{
     console.log("Server is running on port http://localhost:3000")
