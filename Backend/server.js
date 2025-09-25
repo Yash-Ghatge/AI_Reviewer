ac1ba4844393869779e155e4e0986970953e8bd5
Yash-Ghatge <yashghatge012@gmail.com>
2025-09-25 18:34:06 +0530

    Update

--- Backend/server.js
+++ Backend/server.js
@@ -4,9 +4,12 @@ import router from './src/routes/Route.js'
 import cors from 'cors'
 
 const app = express()
-// const allowedOrigins = ['http://localhost:5173','https://ai-reviewer-j876.vercel.app','https://ai-reviewer-j876-git-main-yash-ghatges-projects.vercel.app']
+const allowedOrigins = ['http://localhost:5173','https://ai-reviewer-j876.vercel.app','https://ai-reviewer-j876-git-main-yash-ghatges-projects.vercel.app']
 
-app.use(cors());
+app.use(cors({
+    origin:allowedOrigins,
+    credentials:true
+}));
 
 
 app.use(express.json())
