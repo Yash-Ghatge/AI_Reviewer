0ca14ff4bb43cdf36defbcaf571e0f34dff46dc7
Yash-Ghatge <yashghatge012@gmail.com>
2025-09-25 19:35:12 +0530

    Update

--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,10 @@ import dotenv from 'dotenv'
 import express from 'express'
 import router from './src/routes/Route.js'
 import cors from 'cors'
-
-const app = express()
-const allowedOrigins = ['http://localhost:5173','https://ai-reviewer-j876.vercel.app','https://ai-reviewer-j876-git-main-yash-ghatges-projects.vercel.app']
-
-app.use(express.json())
 dotenv.config()
+const app = express()
+app.use(express.json())
+const allowedOrigins = ['http://localhost:5173','https://ai-reviewer-j876.vercel.app','https://ai-reviewer-j876-git-main-yash-ghatges-projects.vercel.app']
 
 app.use(cors({
     origin:allowedOrigins,
