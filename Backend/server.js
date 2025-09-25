481d6eed74ac0fe79c35b80f975e1385513d0343
Yash-Ghatge <yashghatge012@gmail.com>
2025-09-25 18:26:52 +0530

    Update

--- Backend/server.js
+++ Backend/server.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 
-
+app.use('/api/ai',router)
 app.get('/',(req,res)=>{
     res.send("API IS RUNNING")
 })
-app.use('/api/ai',router)
+
 app.listen(process.env.PORT, ()=>{
     console.log("Server is running on port http://localhost:3000")
 })
