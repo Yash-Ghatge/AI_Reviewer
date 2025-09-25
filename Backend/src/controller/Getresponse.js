7c4a752727ff2d36eeb88d9d373f4729db50aa78
Yash-Ghatge <yashghatge012@gmail.com>
2025-09-25 20:08:10 +0530

    Update

--- Backend/src/controller/Getresponse.js
+++ Backend/src/controller/Getresponse.js
@@ -1,8 +1,9 @@
 import main from '../services/Service.js'
 
 export const getReview = async (req,res) => {
-    
+    console.log(req.body)
     const {code} = req.body
+    
 
     if(!code){
         return res.status(400).json({message:"Prompt is required"})
