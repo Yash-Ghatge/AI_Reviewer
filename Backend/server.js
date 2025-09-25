b044013c6ce12cb37f564d71ac4abe72cf119307
Yash-Ghatge <yashghatge012@gmail.com>
2025-09-25 18:31:37 +0530

    Update

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,24 @@
-import app from './src/app.js'
 import dotenv from 'dotenv'
+import express from 'express'
+import router from './src/routes/Route.js'
+import cors from 'cors'
 
+const app = express()
+// const allowedOrigins = ['http://localhost:5173','https://ai-reviewer-j876.vercel.app','https://ai-reviewer-j876-git-main-yash-ghatges-projects.vercel.app']
+
+app.use(cors());
+
+
+app.use(express.json())
 dotenv.config()
 
 
-app.use('/api/ai',router)
+
 app.get('/',(req,res)=>{
     res.send("API IS RUNNING")
 })
 
+app.use('/api/ai',router)
 app.listen(process.env.PORT, ()=>{
     console.log("Server is running on port http://localhost:3000")
 })
